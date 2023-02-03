de8507f39c4527c1d0fd862b749cfc82228235be
hezefeng <hezefeng@cf.cn>
2023-02-03 16:15:54 +0800

    update README.md

--- go-protoc-example/main.go
+++ go-protoc-example/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/goprotobufexample/person"
+	"google.golang.org/protobuf/proto"
+)
 
+func main() {
+	person1 := person.Person{}
+	person1.PersonName = "feng"
+	person1.PersonAge = 18
+	person1.PersonAddress = "China"
+	person1.PersonId = 1
+
+	bytes, _ := json.Marshal(person1)
+	fmt.Println("bytes =", bytes, " \n", string(bytes))
+
+	out, err := proto.Marshal(&person1)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println("out = ", out)
+
+	person2 := person.Person{}
+	err = proto.Unmarshal(out, &person2)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	bytes, _ = json.Marshal(person2)
+	fmt.Println(string(bytes))
 }

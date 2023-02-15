8b39fe8db442674b2d4afbc61506fea4caf86925
hezefeng <hzf1998@126.com>
2023-02-15 11:01:06 +0800

    update README.md

--- go-grpc-interceptor/client/main.go
+++ go-grpc-interceptor/client/main.go
@@ -6,7 +6,10 @@ import (
 	pb "github.com/grpcinterceptor/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
 	"log"
+	"runtime"
+	"time"
 )
 
 func main() {
@@ -14,7 +17,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("connect failure", err)
 	}
-	conn, err := grpc.Dial("127.0.0.1:9097", grpc.WithTransportCredentials(creds))
+	conn, err := grpc.Dial("127.0.0.1:9097", grpc.WithTransportCredentials(creds), grpc.WithUnaryInterceptor(LogUnaryClientIntercrptor()))
 	if err != nil {
 		log.Fatalf("creds failure", err)
 	}
@@ -28,4 +31,24 @@ func main() {
 		fmt.Println("resp", err)
 	}
 	fmt.Println(resp.GetMessage())
+}
+
+func LogUnaryClientIntercrptor() grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req, reply interface{},
+		cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		// 预处理 (pre-processing)
+		start := time.Now()
+
+		//获取正在运行程序的操作系统
+		cos := runtime.GOOS
+		ctx = metadata.AppendToOutgoingContext(ctx, "client-os", cos)
+
+		err := invoker(ctx, method, req, reply, cc, opts...)
+
+		//后处理
+		end := time.Now()
+		log.Printf("RPC:%s, client-OS: %v req: %v start time: %s , end time : %s err: %v", method, cos, req, start.Format(time.RFC3339), end.Format(time.RFC3339), err)
+		return err
+	}
+
 }
--- go-grpc-interceptor/server/main.go
+++ go-grpc-interceptor/server/main.go
@@ -6,8 +6,11 @@ import (
 	pb "github.com/grpcinterceptor/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
 	"log"
 	"net"
+	"time"
 )
 
 type service struct {
@@ -27,7 +30,8 @@ func main() {
 
 	creds, err := credentials.NewServerTLSFromFile("./key/server.pem", "./key/server.key")
 
-	grpcserver := grpc.NewServer(grpc.Creds(creds), grpc.UnaryInterceptor(LoggingInterceptor))
+	grpcserver := grpc.NewServer(grpc.Creds(creds), grpc.UnaryInterceptor(LogUnaryServerInterceptor()))
+	//grpcserver := grpc.NewServer(grpc.Creds(creds), grpc.UnaryInterceptor(LoggingInterceptor), grpc.UnaryInterceptor(LogUnaryServerInterceptor()))
 	pb.RegisterHelloServer(grpcserver, &service{})
 
 	grpcserver.Serve(listener)
@@ -43,6 +47,41 @@ func LoggingInterceptor(ctx context.Context, req interface{}, info *grpc.UnarySe
 	handler grpc.UnaryHandler) (interface{}, error) {
 	fmt.Printf("gRPC method: %s, %v \n", info.FullMethod, req)
 	resp, err := handler(ctx, req)
-	fmt.Printf("gRPC method: %s, %v", info.FullMethod, resp)
+	fmt.Printf("gRPC method: %s, %v \n", info.FullMethod, resp)
 	return resp, err
+}
+
+func LogUnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler) (resp interface{}, err error) {
+		// 预处理
+		start := time.Now()
+		//从传入的上下文获取数据
+		md, ok := metadata.FromIncomingContext(ctx)
+		if !ok {
+			return nil, fmt.Errorf("couldn't parse incoming context metadata")
+		}
+		//检索客户端操作系统 ，如果不存在则为空
+		os := md.Get("client-os")
+		// 获取客户端ip地址
+		ip, err := getClientIP(ctx)
+		if err != nil {
+			return nil, err
+		}
+
+		//RPC方法真正的执行逻辑 ,调用RPC方法(invoking RPC method)
+		m, err := handler(ctx, req)
+		end := time.Now()
+		log.Printf("RPC: %s,client-OS: '%v' and IP: '%v' req:%v start time: %s, end time: %s, err: %v", info.FullMethod, os, ip, req, start.Format(time.RFC3339), end.Format(time.RFC3339), err)
+		return m, err
+	}
+}
+
+func getClientIP(ctx context.Context) (string, error) {
+	p, ok := peer.FromContext(ctx)
+	if !ok {
+		return " ", fmt.Errorf("can not parse client ip address")
+	}
+	return p.Addr.String(), nil
+
 }
